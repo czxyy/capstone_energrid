d5089c583fa619bd827baf907faebab8898f005c
chloe zheng <chloezxy@hotmail.com>
2019-11-10 11:27:25 +0800

    second commit

---
+++ src/Workflowpages/Workflowpage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+
+function Workflowpage(){
+    return(
+    <>
+    <Container>
+        <div>HELLO HU DIS</div>
+        </Container>
+    </>
+    );
+}
+
+      
+
+export default Workflowpage;
---
+++ src/views/index-sections/Typography.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+
+// core components
+
+function Typography() {
+  return (
+    <>
+      <div className="section">
+        <Container>
+          <h3 className="title">Typography</h3>
+          
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Typography;
